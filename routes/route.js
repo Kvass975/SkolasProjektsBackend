8be2d4919df9bd79526716f85c63fa96e125f182
Roberts Gutmanis <r.gutmanis9751@gmail.com>
2021-11-11 09:12:48 +0200

    delete route

--- routes/route.js
+++ routes/route.js
@@ -5,12 +5,24 @@ const db = require('../models')
 router.get("/", function (req, res) {
     db.Users.find().then(data=>{
         res.json(data)
-    })
-})
-router.post('/', function (req, res) {
-    db.Users.create(req.body).then(data=>{
-        res.send("finished")
+    }).catch(err=>{
+        res.send(err)
     })
 })
 
+router.post('/', function (req, res) {
+    db.Users.create(req.body).then(data=>{
+        res.send("Created")
+    }).catch(err=>{
+        res.send(err)
+    })
+})
+
+router.delete('/:id', function (req, res) {
+    db.Users.remove({_id: req.params.id}).then(data=>{
+        res.send("Deleted")
+    }).catch(err=>{
+        res.send(err)
+    })
+})
 module.exports = router
